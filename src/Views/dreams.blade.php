561dab8aba750efea49b5a5cd3c0893526d2ab90
bimacoding <bimacoding@gmail.com>
2023-01-18 09:26:17 +0700

    Update Dreams

--- src/Views/dreams.blade.php
+++ src/Views/dreams.blade.php
@@ -85,5 +85,6 @@
     <script src="assets/plugins/apexchart/apexcharts.min.js"></script>
     <script src="assets/plugins/apexchart/chart-data.js"></script> --}}
     <script src="assets/js/script.js"></script>
+    @stack('html')
 </body>
 </html>
